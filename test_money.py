7589d3c88db5889f4c7ba6d077586e21a812a9db
Pablo Alcain <pablo.alcain@wildlifestudios.com>
2022-05-06 13:54:39 -0300

    feat: portfolio can sum same currency

--- test_money.py
+++ test_money.py
@@ -9,8 +9,22 @@ class Money:
     def divide(self, divisor):
         return Money(self.amount / divisor, self.currency)
 
+    def add(self, other):
+        return Money(self.amount + other.amount, self.currency)
+
     def __eq__(self, other):
         return self.amount == other.amount and self.currency == other.currency
+
+
+class Portfolio:
+    def __init__(self):
+        self._value = Money(0, "USD")
+
+    def add(self, money):
+        self._value = self._value.add(money)
+
+    def value(self, currency):
+        return self._value
 
 
 class TestMoney:
@@ -25,3 +39,11 @@ class TestMoney:
     def test_division(self):
         original_money = Money(4002, "KRW")
         assert (Money(1000.5, "KRW") == original_money.divide(4))
+
+    def test_sum(self):
+        five_dollars = Money(5, "USD")
+        ten_dollars = Money(10, "USD")
+        portfolio = Portfolio()
+        portfolio.add(five_dollars)
+        portfolio.add(ten_dollars)
+        assert (portfolio.value(currency="USD") == Money(15, "USD"))
